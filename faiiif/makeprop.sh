d075e28c66239b61369c7190b423922f84b042a3
Terry Brady <terrywbrady@gmail.com>
2018-04-18 13:12:28 -0400

    update

--- faiiif/makeprop.sh
+++ faiiif/makeprop.sh
@@ -4,7 +4,7 @@ CANT=$1
 JETTY=$2
 
 echo "CANT:  ${CANT}"
-echo "JETTY: ${JETTY}"
+echo "JETTY: ${JETTY}" 
 
 P=/projects/File-Analyzer-Test-Data/iiif/dog-photos/
 S1="s%https://YOUR-IMAGE-SERVER-URL/project-path%http://${CANT}/iiif/2/dog-photos%g"
